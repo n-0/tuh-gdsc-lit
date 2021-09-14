752cf0c157812aef9c71d4132890d37fd9af6ed0
no <no@localhost.localdomain>
2021-09-14 23:58:04 +0200

    feat: added cards without content to when subpage

--- src/app-when.ts
+++ src/app-when.ts
@@ -1,11 +1,81 @@
-import { LitElement, html } from 'lit'
+import { LitElement, html, css } from 'lit'
 import { customElement } from 'lit/decorators'
+import { card } from './material/card'
 
 
 @customElement('app-when')
 export class AppWhen extends LitElement {
+
+    static styles = [card, css`
+        .when {
+            display: grid;
+            grid-template-rows: 200px 100px 400px;
+            grid-template-columns: 335px 670px 670px 335px;
+            gap: 25px;
+            height: 800px;
+        }
+
+        .title {
+            grid-row: 1;
+            grid-column: 2;
+            font-size: 2.8em;
+            color: #5f6368;
+        }
+
+        .mdc-card {
+            height: 350px;
+            width: 300px;
+        }
+
+        .past {
+            grid-row: 3;
+            grid-column: 2;
+        }
+
+        .present {
+            grid-row: 2 / 3;
+            grid-column: 3;
+        }
+
+        .future {
+            grid-row: 3;
+            grid-column: 4;
+
+        }
+        
+   `]
+
     render() {
-        return html`<div><h1>When</h1></div>`
+        return html`
+        <div class="when">
+            <div class="title">
+                <h1>When</h1>
+            </div>
+            <div class="mdc-card past">
+                <div class="mdc-card__media">
+
+                </div>
+                <div class="mdsc-card__content">
+                    past Event 
+                </div>
+            </div> 
+            <div class="mdc-card present">
+                <div class="mdc-card__media">
+
+                </div>
+                <div class="mdsc-card__content">
+                    present Event 
+                </div>
+            </div> 
+            <div class="mdc-card future">
+                <div class="mdc-card__media">
+
+                </div>
+                <div class="mdsc-card__content">
+                    future Event 
+                </div>
+            </div> 
+        </div>`
     }
 }
 
