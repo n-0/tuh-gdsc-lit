8bcb7ecf2142a627a1c895a2fa196a47910562cb
Denise-Phi Khuu <denisekhuu@hotmail.de>
2021-09-18 16:29:27 +0200

    use flexbox instead of grid

--- src/app-what.ts
+++ src/app-what.ts
@@ -4,35 +4,29 @@ import { fadeAnimations, specialColors } from './styles'
 
 const texts = [
     html`
-    <div class="text-area">
-        <p>Google Developer Student Clubs</p>
-        <p>are <span class="google-green">community</span> groups  for college and</p>
-        <p>university students interested in</p>
-        <p>Google developer technologies.</p>
+    <div class="text-area row">
+        <p>Google Developer Student Clubs are <span class="google-green">community</span> groups  for college and university students interested in Google developer technologies. 
         <p>We are the chapter for the <span class="tuh-blue">TUH</span>!</p>
     </div>
     `,
     html`
-    <div class="text-area">
+    <div class="text-area row">
         <p>Meet students interested in developer technologies at the <span class="tuh-blue">TUH</span>.</p>
         <p>All are welcome, including those with <span class="google-green">diverse</span> backgrounds and different majors.</p>
-        <p>No matter if you feel comfortable in </p>
-        <p>front of a laptop, a construction site or a lab.</p>
+        <p>No matter if you feel comfortable in front of a laptop, a construction site or a lab.</p>
     </div>
     `,
     html`
-    <div class="text-area">
+    <div class="text-area row">
         <p>It is a chance for you to <span class="google-yellow">learn</span> and <span class="google-yellow">grow</span></p> 
         <p>in a peer to peer environment in domains </p>
         <p>that our members are <span class="google-red">passionate</span> about.</p>
     </div>
     `,
     html`
-    <div class="text-area">
-        <p>Through hands-on workshops, events, talks,</p> 
-        <p>and project-building activities - both online and in-person.</p>
-        <p>We strive to <span class="google-blue">solve real life problems</span> and advance our career </p>
-        <p>oriented skills both hard and soft.</p>
+    <div class="text-area row">
+        <p>Through hands-on workshops, events, talks, and project-building activities - both online and in-person.</p>
+        <p>We strive to <span class="google-blue">solve real life problems</span> and advance our career oriented skills both hard and soft.</p>
     </div>
     `
 ]
@@ -66,36 +60,39 @@ export class AppWhat extends LitElement {
 
     static styles = [fadeAnimations, specialColors, 
         css`
+            .flex {
+                display: flex;
+                justify-content: space-around;
+            }
+            .row{
+                flex-direction: row;
+            }
+            .column{
+                flex-direction: column;
+            }
             .what {
-                display: grid;
-                grid-template-rows: 200px 100px 400px;
-                grid-template-columns: 335px 670px 670px 335px;
-                justify-content: center; 
-                gap: 25px;
-                height: 800px;
-                width: 99vw;
+                box-sizing: border-box;
+                height: auto;
+                width: auto;
             }
 
             .title {
+                box-sizing: border-box;
                 display: flex;
-                grid-row: 1;
-                grid-column: 1;
-                justify-content: flex-end;
-                font-size: 2.8em;
+                justify-content: flex-start;
+                padding-left: 1em;
+                font-size: 2em;
                 color: #5f6368;
+                width: 100%;
             }
 
             .text-area {
                 box-sizing: border-box;
-                grid-row: 3;
-                grid-column: 2;
-                font-size: 2em;
+                font-size: 1.5em;
                 animation: fade-in 1.5s cubic-bezier(.35,0,.15,1);
             }
 
             .icon-area {
-                grid-row: 3;
-                grid-column: 3;
                 display: flex;
                 justify-items: center;
                 justify-content: space-evenly;
@@ -118,11 +115,21 @@ export class AppWhat extends LitElement {
             }
 
             .icon-arrow-3 {
-                transform: rotate(-180deg) 
+                transform: rotate(-180deg);
             }
 
             .icon-arrow-4 {
-                transform: rotate(90deg)
+                transform: rotate(90deg);
+            }
+
+            #text-column {
+                width: 50vw;
+                padding: 1em;
+            }
+
+            #icon-column {
+                width: 40vw;
+                padding: 1em;
             }
     `]
 
@@ -131,11 +138,15 @@ export class AppWhat extends LitElement {
 
     render() {
         return html`
-            <div class="what" @click=${() => (this.frame < 3) ? this.frame++ : null}>
             <div class="title"><h1>What?</h1></div>
-            ${texts[this.frame]}
-            ${[0, 1].includes(this.frame) ? icons[0] : icons[1]}
-          </div>
+            <div class="what flex" @click=${() => (this.frame < 3) ? this.frame++ : null}>
+                <div class="column" id="text-column">
+                    ${texts[this.frame]}
+                </div>
+                <div class="column" id="icon-column">
+                    ${[0, 1].includes(this.frame) ? icons[0] : icons[1]}
+                </div>
+            </div>
         `
     }
 }
